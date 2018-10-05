0ef650b7c995aff2d530d70a78c1b500f0140d16
yezi <jingjing.ye@dragonsoftbravo.com>
2018-10-05 15:54:09 +0800

    m

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 import './App.scss';
 
+const API_KEY = 'de526f10e1cd92fe1ffb1067a9edd380';
+
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async(e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-       hello
+       <Titles />
+       <Form getWeather={this.getWeather}/>
+       <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
       </div>
     );
   }
---
+++ weather-app/src/components/Weather.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Weather extends Component {
+  render() {
+    return (
+      <div>
+        { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+        {this.props.description && <p>Conditions: {this.props.description}</p>}
+      </div>
+    )
+  }
+}
